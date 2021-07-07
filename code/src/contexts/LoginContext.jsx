4ccfa1ad94532039d2dc2f71a37f7e8d5903b924
NisreenK <86600994+NisreenK@users.noreply.github.com>
2021-07-07 13:23:26 +0300

    Add files via upload

--- code/src/contexts/LoginContext.jsx
+++ code/src/contexts/LoginContext.jsx
@@ -1,1 +1,35 @@
+import { createContext, useState } from "react";
 
+const LoginContext = createContext({
+  uid: "",
+  setUid: (uid) => {},
+  newpsts: [],
+  addpost: (np) => {},
+  numpsts: 0,
+});
+
+export function LoginContextProvider(props) {
+  const [username, setUsername] = useState("1");
+  const [newposts, setNewposts] = useState([]);
+
+  function addpostHandler(np) {
+    setNewposts((prevPosts) => {
+      return prevPosts.concat(np);
+    });
+  }
+
+  const context = {
+    uid: username,
+    setUid: (uid) => setUsername(uid),
+    newpsts: newposts,
+    addpost: addpostHandler,
+    numpsts: newposts.length,
+  };
+  return (
+    <LoginContext.Provider value={context}>
+      {props.children}
+    </LoginContext.Provider>
+  );
+}
+
+export default LoginContext;
